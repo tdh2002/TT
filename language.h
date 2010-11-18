7ec970178c7233e60ab5ca0486baae4b6e0c6764
tdh2002 <1985bbb@163.com>
2010-11-18 17:24:27 +0800

    2010-11-18

---
+++ language.h
@@ -0,0 +1,17 @@
+
+/*345678901234567890123456789012345678901234567890123456789012345678901234567890
+ *      10        20        30        40        50        60        70        80
+ * language.h
+ */
+
+#ifndef __LANGUAGE_H_
+#define __LANGUAGE_H_
+
+
+#include <glib.h>
+
+extern const gchar *content_en10[];
+extern const gchar **content1_en[]; 
+extern const gchar ***content2_en[];
+
+#endif
